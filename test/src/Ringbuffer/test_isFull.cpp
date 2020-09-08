4749b4951e6713228e0fa6601da7910ef0a4f739
Alexander Entinger <cto@lxrobotics.com>
2020-09-08 12:37:27 +0200

    Adding test code for 'RingbufferN::isFull'

---
+++ test/src/Ringbuffer/test_isFull.cpp
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2020 Arduino.  All rights reserved.
+ */
+
+/**************************************************************************************
+ * INCLUDE
+ **************************************************************************************/
+
+#include <catch.hpp>
+
+#include <vector>
+
+#include <RingBuffer.h>
+
+/**************************************************************************************
+ * TEST CODE
+ **************************************************************************************/
+
+TEST_CASE ("'isFull' should return false for empty ring buffer", "[Ringbuffer-isFull-01]")
+{
+  arduino::RingBufferN<2> ringbuffer;
+  REQUIRE(ringbuffer.isFull() == false);
+}
+
+TEST_CASE ("'isFull' should return true for full ring buffer", "[Ringbuffer-isFull-02]")
+{
+  arduino::RingBufferN<2> ringbuffer;
+  ringbuffer.store_char('A');
+  ringbuffer.store_char('B');
+  REQUIRE(ringbuffer.isFull() == true);
+}
