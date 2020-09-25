99519414caa3bc2a400571810d691b7a7715dd5b
Alexander Entinger <cto@lxrobotics.com>
2020-09-25 08:00:07 +0200

    Adding test code for 'IPAddress::operator !=(...)'

--- test/src/IPAddress/test_operator_comparison.cpp
+++ test/src/IPAddress/test_operator_comparison.cpp
@@ -39,3 +39,15 @@ TEST_CASE ("Testing IPAddress::operator == (IPAddress ip1, uint8_t const * ip2) 
   uint8_t const ip2[] = {10,0,0,1};
   REQUIRE((ip1 == ip2) == false);
 }
+
+TEST_CASE ("Testing IPAddress::operator != (IPAddress ip1, uint8_t const * ip2) with ip1 != ip2", "[IPAddress-Operator-==-05]")
+{
+  arduino::IPAddress ip1(129,168,1,2), ip2(10,0,0,1);
+  REQUIRE((ip1 != ip2) == true);
+}
+
+TEST_CASE ("Testing IPAddress::operator != (IPAddress ip1, uint8_t const * ip2) with ip1 == ip2", "[IPAddress-Operator-==-05]")
+{
+  arduino::IPAddress ip1(129,168,1,2), ip2(129,168,1,2);
+  REQUIRE((ip1 != ip2) == false);
+}
