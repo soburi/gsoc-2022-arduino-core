b76e0afa49257e24a5b2d8b06ae4848aba9646b6
Alexander Entinger <cto@lxrobotics.com>
2020-10-07 17:37:38 +0200

    Adding test code for String::toInt()

---
+++ test/src/String/test_toInt.cpp
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2020 Arduino.  All rights reserved.
+ */
+
+/**************************************************************************************
+ * INCLUDE
+ **************************************************************************************/
+
+#include <catch.hpp>
+
+#include <String.h>
+
+/**************************************************************************************
+ * TEST CODE
+ **************************************************************************************/
+
+TEST_CASE ("Testing String::toInt when string is empty", "[String-toInt-01]")
+{
+  arduino::String str;
+  int const val = str.toInt();
+  REQUIRE(val == 0);
+}
+
+TEST_CASE ("Testing String::toInt when string contains no number", "[String-toInt-02]")
+{
+  arduino::String str("abc");
+  int const val = str.toInt();
+  REQUIRE(val == 0);
+}
+
+TEST_CASE ("Testing String::toInt when string contains a number", "[String-toInt-03]")
+{
+  arduino::String str("-1");
+  int const val = str.toInt();
+  REQUIRE(val == -1);
+}
