dd4467c293cab0b68c3db6844618ef68785b3067
Alexander Entinger <cto@lxrobotics.com>
2020-11-02 14:30:11 +0100

    Adding test code for print(long/unsigned long/long long/unsigned long long)

--- test/src/Print/test_print.cpp
+++ test/src/Print/test_print.cpp
@@ -47,3 +47,51 @@ TEST_CASE ("Print::print(unsigned int, int = DEC|HEX|OCT|BIN)", "[Print-print-03
   WHEN("OCT") { mock.print(val, OCT); REQUIRE(mock._str  == "21"); }
   WHEN("BIN") { mock.print(val, BIN); REQUIRE(mock._str  == "10001"); }
 }
+
+TEST_CASE ("Print::print(long, int = DEC|HEX|OCT|BIN)", "[Print-print-04]")
+{
+  PrintMock mock;
+
+  long const val = -1;
+
+  WHEN("DEC") { mock.print(val, DEC); REQUIRE(mock._str  == "-1"); }
+  WHEN("HEX") { mock.print(val, HEX); REQUIRE(mock._str  == "FFFFFFFFFFFFFFFF"); }
+  WHEN("OCT") { mock.print(val, OCT); REQUIRE(mock._str  == "1777777777777777777777"); }
+  WHEN("BIN") { mock.print(val, BIN); REQUIRE(mock._str  == "1111111111111111111111111111111111111111111111111111111111111111"); }
+}
+
+TEST_CASE ("Print::print(unsigned long, int = DEC|HEX|OCT|BIN)", "[Print-print-04]")
+{
+  PrintMock mock;
+
+  unsigned long const val = 17;
+
+  WHEN("DEC") { mock.print(val, DEC); REQUIRE(mock._str  == "17"); }
+  WHEN("HEX") { mock.print(val, HEX); REQUIRE(mock._str  == "11"); }
+  WHEN("OCT") { mock.print(val, OCT); REQUIRE(mock._str  == "21"); }
+  WHEN("BIN") { mock.print(val, BIN); REQUIRE(mock._str  == "10001"); }
+}
+
+TEST_CASE ("Print::print(long long, int = DEC|HEX|OCT|BIN)", "[Print-print-05]")
+{
+  PrintMock mock;
+
+  long long const val = -1;
+
+  WHEN("DEC") { mock.print(val, DEC); REQUIRE(mock._str  == "-1"); }
+  WHEN("HEX") { mock.print(val, HEX); REQUIRE(mock._str  == "FFFFFFFFFFFFFFFF"); }
+  WHEN("OCT") { mock.print(val, OCT); REQUIRE(mock._str  == "1777777777777777777777"); }
+  WHEN("BIN") { mock.print(val, BIN); REQUIRE(mock._str  == "1111111111111111111111111111111111111111111111111111111111111111"); }
+}
+
+TEST_CASE ("Print::print(unsigned long long, int = DEC|HEX|OCT|BIN)", "[Print-print-06]")
+{
+  PrintMock mock;
+
+  unsigned long long const val = 17;
+
+  WHEN("DEC") { mock.print(val, DEC); REQUIRE(mock._str  == "17"); }
+  WHEN("HEX") { mock.print(val, HEX); REQUIRE(mock._str  == "11"); }
+  WHEN("OCT") { mock.print(val, OCT); REQUIRE(mock._str  == "21"); }
+  WHEN("BIN") { mock.print(val, BIN); REQUIRE(mock._str  == "10001"); }
+}
