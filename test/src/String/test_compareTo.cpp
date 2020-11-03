1cf220b20fd8af727859f89aeaa6014f4d5e20b1
giulcioffi <g.cioffi@arduino.cc>
2020-11-03 17:47:48 +0100

    Add test case for String::compareTo() with empty buffer

--- test/src/String/test_compareTo.cpp
+++ test/src/String/test_compareTo.cpp
@@ -55,3 +55,46 @@ TEST_CASE ("Testing String::compareTo(const char *)", "[String-compareTo-02]")
     REQUIRE(str1.compareTo("Hello") == strcmp(str1.c_str(), str2.c_str()));
   }
 }
+
+TEST_CASE ("Testing String::compareTo(const char *) with empty buffer", "[String-compareTo-03]")
+{
+  WHEN ("First string has a valid buffer")
+  {
+    char *buffer = NULL;
+
+    arduino::String str1("Hello");
+    REQUIRE(str1.compareTo(buffer) != 0);
+  }
+
+  WHEN ("First string does NOT have a valid buffer")
+  {
+    char *buffer1 = NULL;
+    char *buffer2 = NULL;
+
+    arduino::String str1(buffer1);
+    REQUIRE(str1.compareTo(buffer2) == 0);
+  }
+}
+
+
+TEST_CASE ("Testing String::compareTo(const String &) with empty buffer", "[String-compareTo-04]")
+{
+  WHEN ("First string has a valid buffer")
+  {
+    char *buffer = NULL;
+
+    arduino::String str1("Hello");
+    arduino::String str2(buffer);
+    REQUIRE(str1.compareTo(str2) != 0);
+  }
+
+  WHEN ("First string does NOT have a valid buffer")
+  {
+    char *buffer1 = NULL;
+    char *buffer2 = NULL;
+
+    arduino::String str1(buffer1);
+    arduino::String str2(buffer2);
+    REQUIRE(str1.compareTo(str2) == 0);
+  }
+}
