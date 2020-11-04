d2c8b2efbe5afaf6ec73c699efb76986b53cf581
giulcioffi <g.cioffi@arduino.cc>
2020-11-04 17:26:47 +0100

    Add test code for & String::operator[]

--- test/src/String/test_characterAccessFunc.cpp
+++ test/src/String/test_characterAccessFunc.cpp
@@ -49,3 +49,18 @@ TEST_CASE ("Testing String::getBytes(unsigned char, unsigned int, unsigned int)"
     REQUIRE(buf[1] == 'o');
   }
 }
+
+TEST_CASE ("Testing & String::operator[]", "[String-&operator[]-03]")
+{
+  arduino::String str("Hello");
+  str[0] = 'M';
+  REQUIRE(str == "Mello");
+}
+
+TEST_CASE ("Testing & String::operator[] with invalid buffer", "[String-&operator[]-04]")
+{
+  char *buffer = NULL;
+  arduino::String str(buffer);
+  str[0] = 'M';
+  REQUIRE(str[0] == 0);
+}
