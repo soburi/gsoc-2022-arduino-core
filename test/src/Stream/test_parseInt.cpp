da7100c86e101c9a225e2d5b6ea0b9e2722fcdd1
Alexander Entinger <cto@lxrobotics.com>
2020-11-05 12:43:32 +0100

    Reworking parseInt test code

--- test/src/Stream/test_parseInt.cpp
+++ test/src/Stream/test_parseInt.cpp
@@ -18,17 +18,22 @@ TEST_CASE ("Testing parseInt(LookaheadMode lookahead = SKIP_ALL, char ignore = N
 {
   StreamMock mock;
 
-  WHEN ("Only a integer is contained in stream")
+  WHEN ("A positive integer is contained in stream")
   {
     mock << "1234";
     REQUIRE(mock.parseInt() == 1234);
   }
-  WHEN ("The integer is prepended by digits")
+  WHEN ("A negative integer is contained in stream")
+  {
+    mock << "-1234";
+    REQUIRE(mock.parseInt() == -1234);
+  }
+  WHEN ("A integer is prepended by digits")
   {
     mock << "abcdef1234";
     REQUIRE(mock.parseInt() == 1234);
   }
-  WHEN ("The integer is prepended by whitespace chars")
+  WHEN ("A integer is prepended by whitespace chars")
   {
     mock << "\r\n\t 1234";
     REQUIRE(mock.parseInt() == 1234);
@@ -39,19 +44,19 @@ TEST_CASE ("Testing parseInt(LookaheadMode lookahead = SKIP_NONE, char ignore = 
 {
   StreamMock mock;
 
-  WHEN ("Only a integer is contained in stream")
+  WHEN ("A positive integer is contained in stream")
   {
     mock << "1234";
     REQUIRE(mock.parseInt(SKIP_NONE) == 1234);
     REQUIRE(mock.readString() == arduino::String(""));
   }
-  WHEN ("The integer is prepended by digits")
+  WHEN ("A integer is prepended by digits")
   {
     mock << "abcdef1234";
     REQUIRE(mock.parseInt(SKIP_NONE) == 0);
     REQUIRE(mock.readString() == arduino::String("abcdef1234"));
   }
-  WHEN ("The integer is prepended by whitespace chars")
+  WHEN ("A integer is prepended by whitespace chars")
   {
     mock << "\r\n\t 1234";
     REQUIRE(mock.parseInt(SKIP_NONE) == 0);
@@ -63,19 +68,7 @@ TEST_CASE ("Testing parseInt(LookaheadMode lookahead = SKIP_WHITESPACE, char ign
 {
   StreamMock mock;
 
-  WHEN ("Only a integer is contained in stream")
-  {
-    mock << "1234";
-    REQUIRE(mock.parseInt(SKIP_WHITESPACE) == 1234);
-    REQUIRE(mock.readString() == arduino::String(""));
-  }
-  WHEN ("The integer is prepended by digits")
-  {
-    mock << "abcdef1234";
-    REQUIRE(mock.parseInt(SKIP_WHITESPACE) == 0);
-    REQUIRE(mock.readString() == arduino::String("abcdef1234"));
-  }
-  WHEN ("The integer is prepended by whitespace chars")
+  WHEN ("A integer is prepended by whitespace chars")
   {
     mock << "\r\n\t 1234";
     REQUIRE(mock.parseInt(SKIP_WHITESPACE) == 1234);
@@ -87,22 +80,22 @@ TEST_CASE ("Testing parseInt(LookaheadMode lookahead = SKIP_ALL, char ignore = '
 {
   StreamMock mock;
 
-  WHEN ("Only a integer is contained in stream")
+  WHEN ("A positive integer is contained in stream")
   {
     mock << "1234";
     REQUIRE(mock.parseInt(SKIP_ALL, 'a') == 1234);
     REQUIRE(mock.readString() == arduino::String(""));
   }
-  WHEN ("The integer contains only ignore char values")
+  WHEN ("A integer contains only ignore char values")
   {
     mock << "12a3a4a";
-    REQUIRE(mock.parseInt(SKIP_NONE, 'a') == 1234);
+    REQUIRE(mock.parseInt(SKIP_ALL, 'a') == 1234);
     REQUIRE(mock.readString() == arduino::String(""));
   }
-  WHEN ("The integer contains other than ignore chars")
+  WHEN ("A integer contains other than ignore chars")
   {
     mock << "1bed234";
-    REQUIRE(mock.parseInt(SKIP_NONE, 'a') == 1);
+    REQUIRE(mock.parseInt(SKIP_ALL, 'a') == 1);
     REQUIRE(mock.readString() == arduino::String("bed234"));
   }
 }
