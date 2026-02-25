00ca28ff8af729770ec03a8a3e96776d0ccad22e
TOKITA Hiroshi <tokita.hiroshi@gmail.com>
2026-02-25 21:02:27 +0900

    x

--- alt_core_api/tools/protoc_gen_arduinoif/common.py
+++ alt_core_api/tools/protoc_gen_arduinoif/common.py
@@ -19,13 +19,6 @@ __all__ = [
     "ServiceModel",
 ]
 
-
-def full_service_name(package_name: str, service_name: str) -> str:
-    if package_name:
-        return f".{package_name}.{service_name}"
-    return f".{service_name}"
-
-
 class MethodSpec(NamedTuple):
     decl: str
     call_name: str
@@ -36,14 +29,12 @@ class MethodSpec(NamedTuple):
     emit_service: bool
     visibility: str
 
-
 class PlannedMethod(NamedTuple):
     spec: MethodSpec
     in_ifc: bool
     in_api: bool
     in_service: bool
     in_service_impl: bool
-
 
 class ServiceModel(NamedTuple):
     include_list: List[str]
@@ -67,6 +58,10 @@ class ServiceModel(NamedTuple):
     generate_service: bool
     generate_service_impl: bool
 
+def full_service_name(package_name: str, service_name: str) -> str:
+    if package_name:
+        return f".{package_name}.{service_name}"
+    return f".{service_name}"
 
 def _load_module_from_path(source_path: Path, module_name: str) -> types.ModuleType:
     spec = importlib.util.spec_from_file_location(module_name, source_path)
@@ -75,7 +70,6 @@ def _load_module_from_path(source_path: Path, module_name: str) -> types.ModuleT
     module = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(module)
     return module
-
 
 @lru_cache(maxsize=1)
 def get_arduino_opts_pb2() -> types.ModuleType:
