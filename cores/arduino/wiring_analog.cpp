4743b6bece5b89707bba3e57dc477e95ce1d47e9
TOKITA Hiroshi <tokita.hiroshi@gmail.com>
2026-05-05 08:12:12 +0900

    cores: Generate ADC/PWM config from connector definition
    Like digital pins, ADCs and PWMs are also defined from connectors.
    
    Signed-off-by: TOKITA Hiroshi <tokita.hiroshi@gmail.com>

--- cores/arduino/wiring_analog.cpp
+++ cores/arduino/wiring_analog.cpp
@@ -11,17 +11,12 @@
 
 using namespace zephyr::arduino;
 
-#define PWM_DT_SPEC(n, p, i) PWM_DT_SPEC_GET_BY_IDX(n, i),
-#define PWM_PINS(n, p, i)                                                                          \
-	DIGITAL_PIN_GPIOS_FIND_PIN(DT_REG_ADDR(DT_PHANDLE_BY_IDX(DT_PATH(zephyr_user), p, i)),         \
-							   DT_PHA_BY_IDX(DT_PATH(zephyr_user), p, i, pin)),
-
-#define ADC_DT_SPEC(n, p, i) ADC_DT_SPEC_GET_BY_IDX(n, i),
-#define ADC_PINS(n, p, i)                                                                          \
-	DIGITAL_PIN_GPIOS_FIND_PIN(DT_REG_ADDR(DT_PHANDLE_BY_IDX(DT_PATH(zephyr_user), p, i)),         \
-							   DT_PHA_BY_IDX(DT_PATH(zephyr_user), p, i, pin)),
 #define ADC_CH_CFG(n, p, i) arduino_adc[i].channel_cfg,
-
+#define ADC_CONN_CHANNEL_CFG(n, p, i)                                                              \
+	COND_CODE_1(DT_NODE_HAS_STATUS_OKAY(DT_MAP_ENTRY_PARENT_BY_IDX(n, p, i)),                  \
+	            (ADC_CHANNEL_CFG_DT(ADC_CHANNEL_DT_NODE(DT_MAP_ENTRY_PARENT_BY_IDX(n, p, i),   \
+					DT_MAP_ENTRY_PARENT_SPECIFIER_BY_IDX(n, p, i, 0))),),      \
+		    ())
 #define DAC_NODE       DT_PHANDLE(DT_PATH(zephyr_user), dac)
 #define DAC_RESOLUTION DT_PROP(DT_PATH(zephyr_user), dac_resolution)
 #define DAC_CHANNEL_DEFINE(n, p, i)                                                                \
@@ -36,13 +31,6 @@ namespace {
 
 #ifdef CONFIG_PWM
 
-const struct pwm_dt_spec arduino_pwm[] = {
-	DT_FOREACH_PROP_ELEM(DT_PATH(zephyr_user), pwms, PWM_DT_SPEC)};
-
-/* pwm-pins node provides a mapping digital pin numbers to pwm channels */
-const pin_size_t arduino_pwm_pins[] = {
-	DT_FOREACH_PROP_ELEM(DT_PATH(zephyr_user), pwm_pin_gpios, PWM_PINS)};
-
 size_t pwm_pin_index(pin_size_t pinNumber) {
 	for (size_t i = 0; i < ARRAY_SIZE(arduino_pwm_pins); i++) {
 		if (arduino_pwm_pins[i] == pinNumber) {
@@ -56,15 +44,13 @@ size_t pwm_pin_index(pin_size_t pinNumber) {
 
 #ifdef CONFIG_ADC
 
-const struct adc_dt_spec arduino_adc[] = {
-	DT_FOREACH_PROP_ELEM(DT_PATH(zephyr_user), io_channels, ADC_DT_SPEC)};
-
-/* io-channel-pins node provides a mapping digital pin numbers to adc channels */
-const pin_size_t arduino_analog_pins[] = {
-	DT_FOREACH_PROP_ELEM(DT_PATH(zephyr_user), adc_pin_gpios, ADC_PINS)};
-
 struct adc_channel_cfg channel_cfg[] = {
-	DT_FOREACH_PROP_ELEM(DT_PATH(zephyr_user), io_channels, ADC_CH_CFG)};
+#if DT_NODE_HAS_PROP(DT_PATH(zephyr_user), io_channels)
+	DT_FOREACH_PROP_ELEM(DT_PATH(zephyr_user), io_channels, ADC_CH_CFG)
+#elif defined(ZARD_ADC_CONNECTOR)
+	DT_FOREACH_MAP_ENTRY(DT_NODELABEL(ZARD_ADC_CONNECTOR), io_channel_map, ADC_CONN_CHANNEL_CFG)
+#endif
+};
 
 size_t analog_pin_index(pin_size_t pinNumber) {
 	for (size_t i = 0; i < ARRAY_SIZE(arduino_analog_pins); i++) {
@@ -100,7 +86,8 @@ int _analog_write_resolution = 8;
 // Note: We can not update the arduino_adc structure as it is read only...
 static int read_resolution = 10;
 
-uint32_t map64(uint32_t x, uint32_t in_min, uint32_t in_max, uint32_t out_min, uint32_t out_max) {
+__maybe_unused uint32_t map64(uint32_t x, uint32_t in_min, uint32_t in_max, uint32_t out_min,
+							  uint32_t out_max) {
 	return ((uint64_t)(x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min);
 }
 
